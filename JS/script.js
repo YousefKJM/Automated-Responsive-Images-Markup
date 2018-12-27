06d637234835f5ad6c365d1ac5cab5f3147e5f56
Yousefkjm <uaso0of056@gmail.com>
2018-12-27 23:59:19 +0300

    create object containing different sizes values

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,11 @@
 const IMAGES = document.querySelectorAll("img");
+const SIZES = {
+  showcase: "100vw",
+  reason: "(max-width: 799px) 100vw, 372px",
+  feature: "(max-width: 799px) 100vw, 558px",
+  story: "(max-width: 799px) 100vw, 670px",
+
+}
 
 
 function makeSrcset(imgSrc) {
@@ -23,5 +30,6 @@ for (let i = 0; i < IMAGES.length; i++) {
 
 
   let type = IMAGES[i].getAttribute("data-type");
-  console.log(type);
+  let sizes = SIZES[type];
+  console.log(sizes);
 }
